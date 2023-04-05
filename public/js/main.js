98abce617657b04c937d1f171a5784c398464af0
MSmith <marie.ellen@gmail.com>
2023-04-05 17:08:21 -0400

    Dark mode functionality added

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,4 @@
+// Postures menu dropdown:
 document.addEventListener('DOMContentLoaded', function() {
     let elems = document.querySelectorAll('.dropdown-trigger');
     let unicorn = M.Dropdown.init(elems);
@@ -5,3 +6,38 @@ document.addEventListener('DOMContentLoaded', function() {
 
     instance.open();
   });
+
+// Toggle dark mode:
+let darkMode = localStorage.getItem('darkMode');
+
+const darkModeToggle = document.querySelector('#dark-mode-toggle')
+
+// check if dark mode is enabled
+// if enabled, turn it off
+// if enable, turn it on
+
+const enableDarkMode = () => {
+  // add class darkmode to the body
+  document.body.classList.add('darkmode')
+  localStorage.setItem('darkMode', 'enabled')
+}
+
+const disableDarkMode = () => {
+  // add class darkmode to the body
+  document.body.classList.remove('darkmode')
+  localStorage.setItem('darkMode', null)
+}
+
+if (darkMode === 'enabled') {
+  enableDarkMode();
+}
+
+darkModeToggle.addEventListener('click', () => {
+  darkMode = localStorage.getItem('darkMode');
+  if (darkMode !== 'enabled') {
+    enableDarkMode();
+    console.log(darkMode)
+  } else {
+    disableDarkMode();
+  }
+})
