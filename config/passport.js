d0f0dc492fd980f70afcab628a588a4b28f4df0e
MSmith <marie.ellen@gmail.com>
2023-04-22 13:05:06 -0400

    adjusted scope to 'openid email'

--- config/passport.js
+++ config/passport.js
@@ -9,7 +9,7 @@ module.exports = function(passport) {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         callbackURL: '/auth/google/callback',
-        scope: ['openid'],
+        scope: ['openid email'],
     },
     async (accessToken, refreshToken, profile, done) => {
         // console.log(profile)
