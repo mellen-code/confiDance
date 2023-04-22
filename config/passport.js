46bf0efcc99bfaeec6205d023e988db4c5a20e8b
MSmith <marie.ellen@gmail.com>
2023-04-22 14:13:01 -0400

    adjusted callbackURL

--- config/passport.js
+++ config/passport.js
@@ -8,7 +8,7 @@ module.exports = function(passport) {
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: 'https://confidance.cyclic.app/auth/google/callback',
+        callbackURL: 'auth/google/callback',
         scope: ['email'],
     },
     async (accessToken, refreshToken, profile, done) => {
