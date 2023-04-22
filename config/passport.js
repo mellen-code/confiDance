b49ab91494c8a74bc9b0e7646b25447294306456
MSmith <marie.ellen@gmail.com>
2023-04-22 16:44:02 -0400

    adding console.log

--- config/passport.js
+++ config/passport.js
@@ -4,7 +4,7 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy
 const mongoose = require('mongoose')
 const User = require('../models/User')
 
-module.exports = function(passport) {
+module.exports = function(passport, callbackURL) {
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
